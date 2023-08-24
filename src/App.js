c92c2383b9c38cfd4ff4ef9e7941fb1f1f70c315
Vikram0904 <132873198+Vikram0904@users.noreply.github.com>
2023-08-24 22:39:52 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './style.css';
 import Vikram from './Vikram.js';
+import React, { useState } from 'react';
 
 export default function App() {
+  let [name, setName] = useState('Lists:');
+  let [tamil, setTamil] = useState('Something');
   let array = [
     'Game Of Thrones',
     'Breaking Bad',
@@ -10,11 +13,35 @@ export default function App() {
     'Sex Education',
     'Stranger Things',
   ];
+  const changeName = (lname) => {
+    name = lname + ' ' + name;
+    setName(name);
+    alert(name);
+  };
+  const newSeries = () => {
+    setTamil(tamil);
+  };
   return (
-    <div>
-      <h1>Assighnment</h1>
-      <p>My Favourite Web Series</p>
-      <Vikram movie={array} />
-    </div>
+    <>
+      <div className="navbar">
+        <button>English</button>
+        <button>Tamil</button>
+        <button>Telugu</button>
+        <button>Malayalam</button>
+        <button>Hindhi</button>
+      </div>
+      <div>
+        <h1>Assighnment</h1>
+        <p>My Favourite Web Series</p>
+        <p>{name}</p>
+        <Vikram movie={array} />
+        <button
+          onClick={() => changeName('These Are My Favourite')}
+          className="btn1"
+        >
+          Done
+        </button>
+      </div>
+    </>
   );
 }
